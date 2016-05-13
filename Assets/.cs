6bd3b0aea26492cc6dd42e1ce4d5dd6371b74201
L1247 <starlove124760@gmail.com>
2016-05-14 04:17:01 +0800

    新增UnityPackage

---
+++ Assets/.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+///
+/// !!! Machine generated code !!!
+///
+/// A class which deriveds ScritableObject class so all its data 
+/// can be serialized onto an asset data file.
+/// 
+[System.Serializable]
+public class Example : ScriptableObject 
+{	
+    [HideInInspector] [SerializeField] 
+    public string sheetName = "";
+    
+    [HideInInspector] [SerializeField] 
+    public string worksheetName = "";
+    
+    [ExposeProperty]
+    public string SheetName 
+    {
+        get { return sheetName; }
+        set { sheetName = value;}
+    }
+    
+    [ExposeProperty]
+    public string WorksheetName
+    {
+        get { return worksheetName; }
+        set { worksheetName = value;}
+    }
+    
+    // Note: initialize in OnEnable() not here.
+    public Data[] dataArray;
+    
+	public List<Data> DataList;
+
+    void OnEnable()
+    {		
+//#if UNITY_EDITOR
+        //hideFlags = HideFlags.DontSave;
+//#endif
+        // Important:
+        //    It should be checked an initialization of any collection data before it is initialized.
+        //    Without this check, the array collection which already has its data get to be null 
+        //    because OnEnable is called whenever Unity builds.
+        // 		
+        if (dataArray == null)
+            dataArray = new Data[0];
+		DataList = new System.Collections.Generic.List<Data>( dataArray );
+    }
+    
+    //
+    // Highly recommand to use LINQ to query the data sources.
+    //
+
+}
---
+++ Assets/Data.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+///
+/// !!! Machine generated code !!!
+/// !!! DO NOT CHANGE Tabs to Spaces !!!
+/// 
+[System.Serializable]
+public class Data
+{
+}
